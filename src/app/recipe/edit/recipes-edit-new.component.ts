6f45deb08b176bf8498cc7f213bbbcfc0f3202d1
Luca Bernstein <luca.bernstein@sobis.com>
2019-08-01 12:00:45 +0200

    Workaround: Instantly add data to table

--- src/app/recipe/edit/recipes-edit-new.component.ts
+++ src/app/recipe/edit/recipes-edit-new.component.ts
@@ -3,9 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe, RecipeIngredient } from '../recipe.class';
 import { AddDialogComponent } from 'src/app/generic-components/ingredients/add-dialog/add-dialog.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatTableDataSource } from '@angular/material';
 import { ShoppingItemsService } from 'src/app/shopping/shopping-items.service';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
     selector: 'app-recipes-edit-new',
@@ -15,7 +16,6 @@ import { of } from 'rxjs';
 export class RecipesEditNewComponent implements OnInit {
 
     recipe: Recipe;
-
     dataSource;
     displayedColumns = ['name'];
 
@@ -42,8 +42,12 @@ export class RecipesEditNewComponent implements OnInit {
                 this.recipe = this.recipeService.findById(parseInt(recipeIdParam)) as Recipe;
             }
         });
+        // this.dataSource = of(this.recipe);
+        // const data = this.dataSource.data;
+        // data.push(event);
+        // this.dataSource.data = data;
+        this.dataSource = new MatTableDataSource(this.recipe.ingredients);
         console.log(this.recipe);
-        this.dataSource = of(this.recipe.ingredients);
     }
 
     saveRecipe() {
@@ -67,10 +71,14 @@ export class RecipesEditNewComponent implements OnInit {
             if (result) {
                 console.log(JSON.stringify(result));
                 this.recipe.addIngredient(result); // Only save to temporary ingredient object
+
+                // The following two lines are a workaround to some kind of bug: Before, Ingredients were not added to the table instantly
+                // Thx to: https://stackoverflow.com/questions/47581267/how-to-add-data-dynamically-to-mat-table-datasource
+                // TODO: Investigate if this could not be solved otherwise.
+                let data = this.dataSource.data;
+                this.dataSource.data = data;
             }
         });
-        console.log(this.dataSource);
-        // this.dataSource = this.recipe.ingredients;
-    }
 
+    }
 }
