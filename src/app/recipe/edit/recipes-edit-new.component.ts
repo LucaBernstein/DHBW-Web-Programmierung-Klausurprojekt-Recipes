9b72da0101bab69e87caf279685ae271dd14919f
Luca Bernstein <luca.bernstein@sobis.com>
2019-08-01 12:11:46 +0200

    Don't write back modified ingredients if not saved yet

--- src/app/recipe/edit/recipes-edit-new.component.ts
+++ src/app/recipe/edit/recipes-edit-new.component.ts
@@ -5,8 +5,6 @@ import { Recipe, RecipeIngredient } from '../recipe.class';
 import { AddDialogComponent } from 'src/app/generic-components/ingredients/add-dialog/add-dialog.component';
 import { MatDialog, MatTableDataSource } from '@angular/material';
 import { ShoppingItemsService } from 'src/app/shopping/shopping-items.service';
-import { of, Observable } from 'rxjs';
-import { DataSource } from '@angular/cdk/table';
 
 @Component({
     selector: 'app-recipes-edit-new',
@@ -15,7 +13,8 @@ import { DataSource } from '@angular/cdk/table';
 })
 export class RecipesEditNewComponent implements OnInit {
 
-    recipe: Recipe;
+    recipe: Recipe; // We only work on a temp object. Saving persists data, else it is dismissed.
+    ingredients: RecipeIngredient[];
     dataSource;
     displayedColumns = ['name'];
 
@@ -28,7 +27,6 @@ export class RecipesEditNewComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.route.fragment
         // Get URL parameter for current recipe
         this.route.paramMap.subscribe(params => {
             // As we are using the same view also for creating new recipes we want to catch that case:
@@ -38,19 +36,16 @@ export class RecipesEditNewComponent implements OnInit {
                 this.recipe = new Recipe(null);
                 this.recipeService.getNewRecipeId();
             } else {
-                // Fill form with data of existing recipe.
-                this.recipe = this.recipeService.findById(parseInt(recipeIdParam)) as Recipe;
+                // Fill form with data of existing recipe. Copy it so state is not persisted.
+                this.recipe = new Recipe(this.recipeService.findById(parseInt(recipeIdParam)));
+                this.ingredients = [...this.recipe.ingredients]; // Copy ingredients in temp array.
             }
         });
-        // this.dataSource = of(this.recipe);
-        // const data = this.dataSource.data;
-        // data.push(event);
-        // this.dataSource.data = data;
-        this.dataSource = new MatTableDataSource(this.recipe.ingredients);
-        console.log(this.recipe);
+        this.dataSource = new MatTableDataSource(this.ingredients);
     }
 
     saveRecipe() {
+        this.recipe.ingredients = this.ingredients; // Persist (modified) ingredients on recipe.
         this.recipeService.saveOrAdd(this.recipe); // Save (or overwrite) recipe
         this.router.navigate(['recipes', this.recipe.id]); // Go to details page after changes have been saved.
         this.shoppingItemsService.addOrCheckAddedIngredients(this.recipe);
@@ -70,7 +65,7 @@ export class RecipesEditNewComponent implements OnInit {
         dialogRef.afterClosed().subscribe(result => {
             if (result) {
                 console.log(JSON.stringify(result));
-                this.recipe.addIngredient(result); // Only save to temporary ingredient object
+                this.addIngredientToIngredientsList(result); // Only save to temporary ingredient object
 
                 // The following two lines are a workaround to some kind of bug: Before, Ingredients were not added to the table instantly
                 // Thx to: https://stackoverflow.com/questions/47581267/how-to-add-data-dynamically-to-mat-table-datasource
@@ -80,5 +75,21 @@ export class RecipesEditNewComponent implements OnInit {
             }
         });
 
+        this.recipe.title = 'Buhuuuuu';
+
+    }
+
+    addIngredientToIngredientsList(newIng: RecipeIngredient) { // Calculate sums for equal ingredients instead of adding both separately
+        let found = false;
+        for (let i = 0; i < this.ingredients.length; i++) {
+            if (this.ingredients[i].name === newIng.name) {
+                found = true;
+                this.ingredients[i].quantity += newIng.quantity;
+                break;
+            }
+        }
+        if (!found) {
+            this.ingredients.push(newIng);
+        }
     }
 }
--- src/app/recipe/recipe.class.ts
+++ src/app/recipe/recipe.class.ts
@@ -14,20 +14,6 @@ export class Recipe {
     author?: string;
     ingredients?: RecipeIngredient[] = [];
 
-    addIngredient(newIng: RecipeIngredient): Recipe {
-        let found = false;
-        for (let i = 0; i < this.ingredients.length; i++) {
-            if (this.ingredients[i].name === newIng.name) {
-                this.ingredients[i].quantity += newIng.quantity;
-                break;
-            }
-        }
-        if (!found) {
-            this.ingredients.push(newIng);
-        }
-        return this;
-    }
-
     constructor(obj) {
         obj && Object.assign(this, obj);
 
